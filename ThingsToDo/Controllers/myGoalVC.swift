88c958b492b10c41b051e60642463ba28388016f
Dayakar Reddy Marri <dayakarreddy@dayakars-macbook-air.local>
2017-11-22 21:53:37 -0500

    added tableview

--- ThingsToDo/Controllers/myGoalVC.swift
+++ ThingsToDo/Controllers/myGoalVC.swift
@@ -10,9 +10,31 @@ import UIKit
 
 class myGoalVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.isHidden = false
+        
+    }
+    @IBAction func goalButtonPressed(_ sender: UIButton) {
+        print("Goal button pressed")
+    }
+}
+extension myGoalVC: UITableViewDelegate , UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "goalCell") as? goalCell else {
+            return UITableViewCell()
+        }
+        cell.configureCell(description: "Workout everyday", termType: "Long Term", goalProgressAmount: 7)
+        return cell
     }
 }
 
